ed9e273d6944726d2fad3885d36d4db6c5b38f89
afighufran <leticiahd132@gmail.com>
2023-04-26 11:35:34 +0700

    Update app.py

--- 1-flask-intro/app.py
+++ 1-flask-intro/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Belajar Flask Dasar"
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
